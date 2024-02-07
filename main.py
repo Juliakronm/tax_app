e4e69578f4eed1c85c9cfa51457fde13581017d2
Juliakronm <juliakronmarker@gmail.com>
2024-02-07 10:15:06 +0100

    tagit bort överföldig fil

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+# Importera de bibliotek vi behöver
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Läs in CSV-filen
 df = pd.read_csv('miljo.csv')
 
+# Skapa funktion
 def show_all():
     # Skapa ett linjediagram
     df.plot(x='Year', y=' Tax', kind='line', marker='o')
-
     # Visa diagrammet
     plt.show()
 
+# Skapa funktion
 def compare_two():
-    # Välj de två år du vill jämföra
+    # Spara år från inputs i variabler
     år1 = input('Skriv ett år du vill kolla på (2011-2021): ')
     år2 = input('Skriv året du vill jämföra det med (2011-2021): ')
-    print(f'{år1} och {år2}')
 
     # Filtrera DataFrame för att få endast de två åren
     selected_years = df[df['Year'].isin([int(år1), int(år2)])]
@@ -23,7 +24,7 @@ def compare_two():
     # Skapa ett stapeldiagram
     selected_years.plot(x='Year', y=' Tax', kind='bar', legend=False)
 
-    # Ange diagramtitel och etiketter
+    # Välj diagramtitel och etiketter
     plt.title(f'Jämförelse av miljöskatt mellan {år1} och {år2}')
     plt.xlabel('År')
     plt.ylabel('Miljöskatt')
@@ -31,20 +32,20 @@ def compare_two():
     # Visa diagrammet
     plt.show()
 
-
+# Skapa funktion
 def compare_three():
-    # Välj de två år du vill jämföra
+    # Spara år från inputs i variabler
     år1 = input('Skriv ett år du vill kolla på (2011-2021): ')
     år2 = input('Skriv andra året du vill jämföra det med (2011-2021): ')
     år3 = input('Skriv tredje året du vill jämföra det med (2011-2021): ')
 
-    # Filtrera DataFrame för att få endast de tre åren
+    # Filtrerar DataFrame för att få endast de tre åren
     selected_years = df[df['Year'].isin([int(år1), int(år2), int(år3)])]
 
-    # Skapa ett stapeldiagram
+    # Skapar ett stapeldiagram
     selected_years.plot(x='Year', y=' Tax', kind='bar', legend=False)
 
-    # Ange diagramtitel och etiketter
+    # Välj diagramtitel och etiketter
     plt.title(f'Jämförelse av miljöskatt mellan {år1}, {år2} och {år3}')
     plt.xlabel('År')
     plt.ylabel('Miljöskatt')
@@ -52,3 +53,7 @@ def compare_three():
     # Visa diagrammet
     plt.show()
 
+# Skapa funktion
+def show_as_list():
+    # Skriv ut CSV-filen
+    print(df)
--- menu.py
+++ menu.py
@@ -1,35 +1,44 @@
-#Importera min fil med funktioner
+# Importera min fil med funktioner
 import main
 
-#Boolean som avgör om programmet körs
+# Boolean som avgör om programmet körs
 run = True
 
-#Loop för att programmet ska vara aktivt enligt föregående variabel.
+# Loop för att programmet ska vara aktivt enligt föregående variabel.
 while run:
     answer = input('Välkommen till denna enkla applikation där du får möjlighet att titta på miljöskatt för privatkonsumption mellan åren 2011-2021. Välj ett av nedan val.'
                     "\n\nMeny:"
                    '\n1. Se miljöskatten som diagram med samtliga år'
                    '\n2. Jämför två år'
                    '\n3. Jämför tre år'
-                   '\n4. tbc'
+                   '\n4. Visa samtliga år i en tabell'
                    '\n5. tbc'
                    '\n6. tbc'
                    '\nQ. Avsluta program'
                    '\n -> ').strip()
+    # Se till att rätt svar leder till rätt funktion
     match answer.lower():
+        # Startar funktion 1 enligt användarens val.
         case '1':
             main.show_all()
+        # Startar funktion 2 enligt användarens val.
         case  '2':
             main.compare_two()
+        # Startar funktion 3 enligt användarens val.
         case '3':
-            print('Oj, här var det tomt. Återkom senare igen.')
+            main.compare_three()
+        # Startar funktion 4 enligt användarens val.
         case '4':
-            print('Oj, här var det tomt. Återkom senare igen.')
+            main.show_as_list()
+        # Startar funktion 5 enligt användarens val.
         case '5':
             print('Oj, här var det tomt. Återkom senare igen.')
+        # Startar funktion 6 enligt användarens val.
         case '6':
             print('Oj, här var det tomt. Återkom senare igen.')
+        # För att avsluta programmet
         case 'q':
             run = False
+        # Om man skriver något som inte finns:
         case _:
             print(f'{answer} är inte ett alternativ, gör om gör rätt.')
